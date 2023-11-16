ce69ada64ede44c5e046f9e8ddef4a6468ab5b88
viniciusdlx <viniciuslx204@hotmail.com>
2023-11-16 01:30:42 -0300

    CHORE: main ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    fs.readdir('./', (err, files) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(files);
+    });
     console.log(__dirname);
     console.log('./../db.sqlite');
     const app = await NestFactory.create(AppModule);
