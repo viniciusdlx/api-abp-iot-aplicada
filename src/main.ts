f2873a6b9756bb30cccbd178c083816c92c7bf1c
viniciusdlx <viniciuslx204@hotmail.com>
2023-11-16 01:23:59 -0300

    CHORE: updated main and vercel file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    console.log(__dirname);
+    console.log('./../db.sqlite');
     const app = await NestFactory.create(AppModule);
 
     app.enableCors();
