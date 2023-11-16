513869eb46519529aefb693434df732b11746ce4
viniciusdlx <viniciuslx204@hotmail.com>
2023-11-16 00:07:00 -0300

    REFACTOR: Add database and user module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { DatabaseModule } from './config/database/database.module';
+import { UserModule } from './modules/users/user.module';
 
 @Module({
-    imports: [],
+    imports: [DatabaseModule, UserModule],
     controllers: [AppController],
     providers: [],
 })
